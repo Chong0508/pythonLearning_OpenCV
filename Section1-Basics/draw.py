a5f099c64a8a8f9a960d8fca65e989345667c59d
munkei <mk.chong5@gmail.com>
2025-10-31 17:27:42 +0800

    Update Draw shape & Write Text

---
+++ Section1-Basics/draw.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+
+# Create a blank image
+blank = np.zeros((500, 500,3), dtype='uint8')
+cv.imshow('Blank', blank)
+
+# 1. Paint the image a certain colour
+# blank[:] = 0,255,0       # Green
+# blank[:] = 0,0,255       # red
+blank[200:300, 300:400] = 0,0,255       # red (pixel)
+cv.imshow('Red', blank)
+
+# 2. Draw a Rectangle
+# cv.rectangle(blank, (0,0), (250,500), (0,255,0) , thickness = 2)
+# cv.rectangle(blank, (0,0), (250,500), (0,255,0) , thickness = cv.FILLED)
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0) , thickness = cv.FILLED)      # show a square 
+cv.imshow('Rectangle', blank)
+
+# 3. Draw a Circle
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=3)
+cv.imshow('Circle',blank) 
+
+# 4. Draw a Line
+cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255) , thickness = 3)
+cv.imshow('Line', blank)
+
+# 5. Write Text
+cv.putText(blank, 'Hello, my name is Jane!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
