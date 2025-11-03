5e0f4fb28fbdf9e97e49d27f40eaf1b788b4bad4
munkei <mk.chong5@gmail.com>
2025-11-03 10:43:17 +0800

    Update Histogram Computation & Thresholding/Binarizing Images

---
+++ Section2-Advanced/thresh.py
@@ -0,0 +1,26 @@
+# Thresholding is a binary realisation of an image
+# Convert an image to a binary image (pixels in zero(black)/one(white))
+
+import cv2 as cv
+
+img = cv.imread('Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Simple Thresholded', thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Simple Thresholded Inverse', thresh_inv)
+
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Thresholding', adaptive_thresh)
+
+adaptive_thresh2 = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Thresholding Gaussian', adaptive_thresh2)
+
+cv.waitKey(0)
